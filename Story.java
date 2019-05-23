bf0bbcbac7a1388eef081b55fb3908e636276734
akinam2001 <anikamahajan.usa@gmail.com>
2019-05-23 13:00:28 -0700

    more debugging

--- Story.java
+++ Story.java
@@ -63,15 +63,15 @@ public class Story
         System.out.println("5 = Pirate - Weapon: Claw");
         String decide = UserInput.getString();
         if(decide.equals("1")) job = "Warrior";
-        else if (decide.equals("2")) job = "Corgi";
-        else if (decide.equals("3")) job = "Mage - Weapon: Staff";
-        else if (decide.equals("4")) job = "Pomeranian";
-        else if (decide.equals("5")) job = "Golden Retriever";
+        else if (decide.equals("2")) job = "Archer";
+        else if (decide.equals("3")) job = "Mage";
+        else if (decide.equals("4")) job = "Thief";
+        else if (decide.equals("5")) job = "Pirate";
         else{
           System.out.println("INVALID INPUT");
           setUp();
         }
-        System.out.println("Are you sure you want to be named "  + name + " the "+ job + " " + puppy + " ? (yes or no)");
+        System.out.println("Are you sure you want to be named "  + name + " the "+ job + " " + puppy + "? (yes or no)");
         String option1 = UserInput.getString();
         if(option1.equals( "yes"))one();
         else setUp();
@@ -80,7 +80,9 @@ public class Story
     {
         boolean valid = true;
         while(valid){
-            System.out.println("One day, while strolling the town square plaza and browsing along the busy markets, " + name + " the " + puppy + " , a canine of humble upbringing spots a poster recruiting anyone brave enough to slay the monster");
+            System.out.println(" ");
+            System.out.println("One day, while strolling the town square plaza and browsing along the busy markets, " + name + " the " + puppy + ", a canine of humble");
+            System.out.println("upbringing, spots a poster recruiting anyone brave enough to slay the monster");
             System.out.println("Recruiter: Recruiting all valiant enough to fight on behalf of King Roofster IV");
             System.out.println("What will you say?");
             System.out.println("Option 1: I shall go for “His Royal Fluffiness”. This is going to be a PAWsitively fun adventure!");
@@ -90,14 +92,19 @@ public class Story
             String choice = UserInput.getString();
             if(choice.equals("1")){
                 valid = false;
+                System.out.println(" ");
                 pathOne();
             }
             else if (choice.equals("2")){
                 valid = false;
+                System.out.println(" ");
+                System.out.println("You failed as a " + job);
                 System.out.println("Game Over");
+                System.out.println(" ");
             }
             else if (choice.equals("3")){
                 valid = false;
+                System.out.println(" ");
                 //pathThree();
             }
             else{
@@ -115,11 +122,14 @@ public class Story
             System.out.println("Roll the die");
             System.out.println(random);
             if(random >= 3){
+                System.out.println(" ");
                 System.out.println("Congratulations, you have been promoted to the rank of Canine Knight!");
                 pathOne_A();
             }
             else{
+                System.out.println(" ");
                 System.out.println("Game Over");
+                System.out.println(" ");
             }
         }
     }
@@ -131,11 +141,14 @@ public class Story
              String choice = UserInput.getString();
             if(choice.equals("1")){
                 path = false;
+                System.out.println(" ");
                 System.out.println("You found nothin");
                 System.out.println("Game Over");
+                System.out.println(" ");
             }
             else if (choice.equals("2")){
                 path = false;
+                System.out.println(" ");
                 //pathOne_A_2();
             }
             else{
@@ -144,13 +157,15 @@ public class Story
         }
         
     }
-    
     public static void info(){
+        System.out.println(" ");
        System.out.println("This game is about the adventures of Sam the Samoyed!");
         System.out.println("He has the chance to fight a huge monster and prove that he is the best of the best!");
         System.out.println("Will you be worthy enough to lead Sam to victory?");
+        System.out.println(" ");
     }
     public static void credits(){
+        System.out.println(" ");
         System.out.println("CREDITS");
         System.out.println("Dogtator: Anika Mahajan");
         System.out.println("Dog Puns Kween: Aria Koul");
@@ -158,5 +173,6 @@ public class Story
         System.out.println("Idea Hero: Allen Cao");
         System.out.println("Creative Paragon: Alex Shen");
         System.out.println("You are PAWsitively amazing wof!");
+        System.out.println(" ");
     }
 }
